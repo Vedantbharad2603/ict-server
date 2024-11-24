a508edd2e37f60a8614f230a56065d5dc547182b
devan <devanbhensdadiya123@gmail.com>
2024-11-24 13:40:29 +0530

    Update Password API Added

---
+++ api/routes/PasswordRoutes.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . '/../controllers/PasswordController.php';
+
+function PasswordRoutes($method, $subpath) {
+    switch ($subpath) {
+        case 'updatePassword': // Handle "Password/updatePassword"
+            if ($method === 'POST') {
+                $input = json_decode(file_get_contents('php://input'), true);
+                UpdatePasswordController($input);
+            } else {
+                http_response_code(405); // Method Not Allowed
+                echo json_encode(['message' => 'Method not allowed']);
+            }
+            break;
+
+        default:
+            http_response_code(404); // Not Found
+            echo json_encode(['message' => 'Invalid Faculty API endpoint']);
+            break;
+    }
+}
